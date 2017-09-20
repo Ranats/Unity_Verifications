f1e81bef6009b150619b3461cc0740d657c613f8
Satoshi Coda <sopurani885@gmail.com>
2017-09-20 13:36:02 +0900

    お前の態度が気に入らない

--- Player.cs
+++ Player.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 static class X{ public static float max, min; }
-static class Z{ static public float max, min; }
+static class Z{ public static float max, min; }
 
 public class Player : MonoBehaviour {
     public Transform plane;
     [Range(0f, 1f)]
     public float speed;
     
-	void Start () {
+　　void Start () {
         X.min = -(10 * plane.localScale.x / 2) + this.transform.localScale.x / 2;
         X.max = (10 * plane.localScale.x / 2) - this.transform.localScale.x / 2;
         Z.min = -(10 * plane.localScale.z / 2) + this.transform.localScale.z / 2;
@@ -22,10 +22,9 @@ public class Player : MonoBehaviour {
         unit.x += Input.GetAxis("Horizontal") * speed;
         unit.z += Input.GetAxis("Vertical") * speed;
         transform.position = Clamp(unit);
-	}
+　　}
 
-    Vector3 Clamp(Vector3 vec)
-    {
+    Vector3 Clamp(Vector3 vec){
         return new Vector3(Mathf.Clamp(vec.x,X.min,X.max),vec.y, Mathf.Clamp(vec.z, Z.min, Z.max));
     }
 }
